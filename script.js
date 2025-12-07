54d97bf7875baa6c74cb2581ca8edd9e1aeb8be3
Rupa Devi <rupadevi7094@gmail.com>
2025-12-07 20:37:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const saveBtn = document.querySelector("input[type='submit']")
+const root = document.documentElement;
+
+const cookies = document.cookie.split(";")
+
+cookies.forEach(cookie => {
+	const cookiee = cookie.split("=");
+	if(cookiee[0] === " fontsize") {
+	root.style.setProperty('--fontsize', cookiee[1]+"px");
+	}
+	if(cookiee[0] === " fontcolor") {
+	root.style.setProperty('--fontcolor', cookiee[1]);
+	}
+})
+
+saveBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	const fontSize = document.getElementById("fontsize").value;
+	const fontColor = document.getElementById("fontcolor").value;
+
+	document.cookie = `fontsize=${fontSize}`
+	document.cookie = `fontcolor=${fontColor}`
+	
+})
